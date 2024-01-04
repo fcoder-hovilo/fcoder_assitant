9b1bb10c32a4b44cb7b9de391718d7578d35b4d5
Kang The Variants <78076796+khengyun@users.noreply.github.com>
2024-01-05 04:23:55 +0700

    Refactor build script to remove unnecessary code

--- app/scarcehost_build.sh
+++ app/scarcehost_build.sh
@@ -6,11 +6,5 @@ binary_name="CHANGE_ME"
 
 apt install libc6
 
-# Check if the build was successful
-if [ $? -eq 0 ]; then
-  echo "Running the program..."
-  # Run the compiled executable
-  ./$binary_name
-else
-  echo "Build failed. Please check your Go code for errors."
-fi
+./$binary_name
+
