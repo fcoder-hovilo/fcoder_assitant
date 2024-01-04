8a94cca0234adc9aacee8bd43b592b6ee56836c8
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-01-04 23:54:01 +0700

    Contents require for new go-task command to work

--- app/dev/open-browser.go
+++ app/dev/open-browser.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
 )
 
 func main() {
@@ -12,13 +14,48 @@ func main() {
 	// Check the operating system
 	switch runtime.GOOS {
 	case "linux":
+		dockerCmd := exec.Command("sudo", "docker-compose", "up", "--build", "-d")
+		if err := runAndWait(dockerCmd); err != nil {
+			fmt.Println("Error running Docker command:", err)
+			os.Exit(1)
+		}
+	case "darwin":
+		// Assume macOS uses the same command as Linux
+		dockerCmd := exec.Command("docker-compose", "up", "--build", "-d")
+		if err := runAndWait(dockerCmd); err != nil {
+			fmt.Println("Error running Docker command:", err)
+			os.Exit(1)
+		}
+	case "windows":
+		dockerCmd := exec.Command("docker-compose", "up", "--build", "-d")
+		if err := runAndWait(dockerCmd); err != nil {
+			fmt.Println("Error running Docker command:", err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Println("Unsupported operating system")
+		os.Exit(1)
+	}
+
+	// Wait for 10 seconds
+	time.Sleep(10 * time.Second)
+
+	// Open the browser
+	switch runtime.GOOS {
+	case "linux":
 		exec.Command("xdg-open", url).Start()
 	case "darwin":
 		exec.Command("open", url).Start()
 	case "windows":
 		exec.Command("cmd", "/c", "start", url).Start()
 	default:
-		println("Unsupported operating system")
+		fmt.Println("Unsupported operating system")
 		os.Exit(1)
 	}
+}
+
+func runAndWait(cmd *exec.Cmd) error {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
